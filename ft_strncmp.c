958bc37b7b7d4b5c29fd9d77560c9b09bca05d2c
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-25 08:59:10 +0200

    added check, so it donen't compare emplty strings

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 16:55:29 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/04/04 17:10:03 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/04/25 08:58:03 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
-	while (i < n)
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
 		if (ptr1[i] != ptr2[i])
 			return (ptr1[i] - ptr2[i]);
