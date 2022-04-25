f7153de940e7bd8e2e6ee852942ec58bda3de8ea
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-25 09:44:10 +0200

    NULL guard

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 12:26:35 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/04/04 14:49:01 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/04/25 09:40:02 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_strdup(const char *s1)
 	i = 0;
 	size = ft_strlen (s1);
 	cp = ((char *) malloc(size + 1));
+	if (cp == NULL)
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		cp[i] = s1[i];
