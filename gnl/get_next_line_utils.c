c6559ed03a39ca6c769830e56884882db8a862d7
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-11-04 09:36:32 +0100

    Removed reductant functions

--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:39:38 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/08/03 20:48:55 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/11/04 09:36:17 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,59 +40,4 @@ char	*move_str(char *dest, char *src, size_t len)
 	while (dest[i] != '\0')
 		dest[i++] = '\0';
 	return (dest);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	const unsigned char	*p_src;
-	unsigned char		*p_dest;
-	size_t				i;
-
-	if (dest == NULL && src == NULL)
-		return (NULL);
-	p_dest = dest;
-	p_src = src;
-	i = 0;
-	while (i < n)
-	{
-		p_dest[i] = p_src[i];
-		i++;
-	}
-	return (dest);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*concat_str;
-	int		length_s1;
-	int		length_s2;
-
-	if (!s1)
-	{
-		s1 = malloc(sizeof(char));
-		if (s1 == NULL)
-			return (NULL);
-		*s1 = '\0';
-	}
-	if (*s2 == '\0')
-		return (s1);
-	length_s1 = ft_strlen(s1);
-	length_s2 = ft_strlen(s2);
-	concat_str = malloc(sizeof(char) * (length_s1 + length_s2) + 1);
-	if (!concat_str)
-		return (NULL);
-	ft_memcpy(concat_str, s1, length_s1);
-	ft_memcpy(concat_str + length_s1, s2, length_s2 + 1);
-	free(s1);
-	return (concat_str);
 }
